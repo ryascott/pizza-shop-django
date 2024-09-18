718d1265aaac6aed5a6e678ccf01164326da0486
Lee Trout <lee@leetrout.com>
2024-09-17 20:56:15 -0400

    Add frontend build action (#5)
    * Add frontend build action
    
    * Guard sentry config

--- config/settings/base.py
+++ config/settings/base.py
@@ -155,16 +155,5 @@ STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-import sentry_sdk
-
-sentry_sdk.init(
-    dsn=env.str("SENTRY_DSN"),
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=env.float("SENTRY_TRACE_SAMPLE_RATE", 1.0),
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=env.float("SENTRY_PROFILE_SAMPLE_RATE", 1.0),
-)
+if env.bool("SENTRY_ENABLED", False):
+    from .sentry import *
