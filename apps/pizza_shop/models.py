e2787dce9142a4d6a44dfcd2561394b86e272037
ryanscott <ryscott@gmail.com>
2024-09-22 15:52:27 -0400

    return to self.name + selfm.email

--- apps/pizza_shop/models.py
+++ apps/pizza_shop/models.py
@@ -36,7 +36,7 @@ class Customer(models.Model):
     date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return self.name + self.email
 
 
 class Topping(models.Model):
